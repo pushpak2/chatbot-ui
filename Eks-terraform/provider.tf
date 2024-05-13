c48e07b62c777119deca7a95fbd763126c5b2a85
Pushpak Jadhav <99756076+pushpak2@users.noreply.github.com>
2024-05-13 15:43:46 +0530

    Update provider.tf

--- Eks-terraform/provider.tf
+++ Eks-terraform/provider.tf
@@ -9,7 +9,7 @@ terraform {
 
 # Configure the AWS Provider
 provider "aws" {
-  region = "ap-south-1"
+  region = "us-east-1"
   access_key = "AKIAYOUC3DFWQRBIIDEV"
   secret_key = "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"
 }
